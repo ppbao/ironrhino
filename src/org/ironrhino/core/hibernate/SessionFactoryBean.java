d479f20d6bdc4b637d55fccd6ceb6d167690cadb
Yanming Zhou <zhouyanming@gmail.com>
2016-08-05 19:02:36 +0800

    Supports Interceptor and EventListners

--- src/org/ironrhino/core/hibernate/SessionFactoryBean.java
+++ src/org/ironrhino/core/hibernate/SessionFactoryBean.java
@@ -16,13 +16,20 @@ import javax.persistence.Converter;
 import javax.persistence.Entity;
 
 import org.apache.commons.lang3.StringUtils;
+import org.hibernate.Interceptor;
 import org.hibernate.MultiTenancyStrategy;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventType;
+import org.hibernate.event.spi.PostDeleteEventListener;
+import org.hibernate.event.spi.PostInsertEventListener;
+import org.hibernate.event.spi.PostUpdateEventListener;
 import org.hibernate.id.factory.IdentifierGeneratorFactory;
+import org.hibernate.internal.SessionFactoryImpl;
 import org.ironrhino.core.hibernate.dialect.MyDialectResolver;
 import org.ironrhino.core.util.ClassScanner;
 import org.slf4j.Logger;
@@ -47,6 +54,18 @@ public class SessionFactoryBean extends org.springframework.orm.hibernate4.Local
 
 	@Autowired(required = false)
 	private CurrentTenantIdentifierResolver currentTenantIdentifierResolver;
+
+	@Autowired(required = false)
+	private Interceptor entityInterceptor;
+
+	@Autowired(required = false)
+	private List<PostInsertEventListener> postInsertEventListeners;
+
+	@Autowired(required = false)
+	private List<PostUpdateEventListener> postUpdateEventListeners;
+
+	@Autowired(required = false)
+	private List<PostDeleteEventListener> postDeleteEventListeners;
 
 	private Class<?>[] annotatedClasses;
 
@@ -114,6 +133,8 @@ public class SessionFactoryBean extends org.springframework.orm.hibernate4.Local
 				setCurrentTenantIdentifierResolver(currentTenantIdentifierResolver);
 			}
 		}
+		if (entityInterceptor != null)
+			setEntityInterceptor(entityInterceptor);
 		super.afterPropertiesSet();
 	}
 
@@ -147,7 +168,22 @@ public class SessionFactoryBean extends org.springframework.orm.hibernate4.Local
 				logger.info(ac.getClass().getName());
 			}
 		}
-		return sfb.buildSessionFactory();
+		SessionFactory sessionFactory = sfb.buildSessionFactory();
+		if (postInsertEventListeners != null || postUpdateEventListeners != null || postDeleteEventListeners != null) {
+			SessionFactoryImpl sf = (SessionFactoryImpl) sessionFactory;
+			EventListenerRegistry registry = sf.getServiceRegistry().getService(EventListenerRegistry.class);
+			if (postInsertEventListeners != null)
+				registry.getEventListenerGroup(EventType.POST_INSERT)
+						.appendListeners(postInsertEventListeners.toArray(new PostInsertEventListener[0]));
+			if (postUpdateEventListeners != null)
+				registry.getEventListenerGroup(EventType.POST_UPDATE)
+						.appendListeners(postUpdateEventListeners.toArray(new PostUpdateEventListener[0]));
+			if (postDeleteEventListeners != null)
+				registry.getEventListenerGroup(EventType.POST_DELETE)
+						.appendListeners(postDeleteEventListeners.toArray(new PostDeleteEventListener[0]));
+		}
+		return sessionFactory;
+
 	}
 
 }
