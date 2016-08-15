c3c2551d1218bfff0825c5091d712ef32cdd0a7c
Yanming Zhou <zhouyanming@gmail.com>
2016-08-15 09:48:29 +0800

    Make field volatile for thread safe

--- src/org/ironrhino/common/support/SettingControl.java
+++ src/org/ironrhino/common/support/SettingControl.java
@@ -41,7 +41,7 @@ public class SettingControl {
 	@Autowired
 	private EntityManager<Setting> entityManager;
 
-	private Map<String, Setting> settings;
+	private volatile Map<String, Setting> settings;
 
 	@PostConstruct
 	public void afterPropertiesSet() {
