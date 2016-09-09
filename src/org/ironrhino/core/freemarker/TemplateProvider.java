2b988841742d23c0321bd1ddc2b8a3b2c2ce584a
Yanming Zhou <zhouyanming@gmail.com>
2016-09-09 11:31:54 +0800

    Create MyMapModel for StaticsModel and refactor

--- src/org/ironrhino/core/freemarker/TemplateProvider.java
+++ src/org/ironrhino/core/freemarker/TemplateProvider.java
@@ -1,4 +1,4 @@
-package org.ironrhino.core.struts;
+package org.ironrhino.core.freemarker;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -66,7 +66,7 @@ public class TemplateProvider {
 	}
 
 	public Map<String, String> getAllSharedVariables() {
-		Map<String, String> allSharedVariables = new HashMap<String, String>(8);
+		Map<String, String> allSharedVariables = new HashMap<>(8);
 		if (StringUtils.isNotBlank(base))
 			allSharedVariables.put("base", base);
 		if (StringUtils.isNotBlank(assetsBase))
